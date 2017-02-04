28fafa781c533be77219f84ac424e3b3d573f607
highhigheggman <highhigheggman@yahoo.co.jp>
2017-02-05 05:10:20 +0900

    first commit

---
+++ raspberryPi/serial.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
+
+import serial
+import serial.tools.list_ports
+
+if __name__ =='__main__':
+    ser = serial.Serial()
+    ser.baudrate = 9600
+
+    # get a list of ports
+    ser_ls = serial.tools.list_ports.comports()
+
+    for i in ser_ls:
+        print i;
+        ser.port = i;
