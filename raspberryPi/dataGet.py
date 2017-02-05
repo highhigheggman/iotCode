c99f382483674f79e52fa2bd2807b576522f4265
highhigheggman <highhigheggman@yahoo.co.jp>
2017-02-05 15:28:00 +0900

    update

--- raspberryPi/dataGet.py
+++ raspberryPi/dataGet.py
@@ -2,14 +2,15 @@
 #! -*- coding: utf-8 -*-
 
 import serial
-import serial.tools.list_ports
 
 if __name__ =='__main__':
     ser = serial.Serial()
-    ser.baudrate = 9600
+    #ser.baudrate = 9600
 
+"""
     # get a list of ports
     ser_ls = serial.tools.list_ports.comports()
 
     for i in ser_ls:
-        print i;
+        print (i);
+        """
