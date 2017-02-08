c8b4a5e3698dbe388ea324f8fbedba5aaffb04d8
highhigheggman <highhigheggman@yahoo.co.jp>
2017-02-08 17:46:31 +0900

    update

--- arduino/serialSensor/serialSensor.ino
+++ arduino/serialSensor/serialSensor.ino
@@ -1,19 +1,16 @@
-#include <avr/sleep.h>
-
-//const int analogPin01 = A0;
+const int analogPin01 = A0;
 int valPin01 = 0;
 
-void setup(){
-    Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
 }
 
-void loop(){
-    //read sensor val
-    //valPin01 = analogRead(analogPin01);
-    valPin01++;
+void loop() {
+  //read sensor val
+  valPin01 = analogRead(analogPin01);
+  
+  //send sensor val
+  Serial.println(valPin01, DEC);
 
-    //send sensor val
-    Serial.write(valPin01);
-
-    delay(3000);
+  delay(100);
 }
--- raspberryPi/dataGet.py
+++ raspberryPi/dataGet.py
@@ -3,14 +3,27 @@
 
 import serial
 import serial.tools.list_ports
+import time
 
 if __name__ =='__main__':
+
+    # wait arduino bootup
+    time.sleep(2.0)
+
     ser = serial.Serial()
     ser.baudrate = 9600
 
     # get a list of ports
-    ser_ls = serial.tools.list_ports.comports()
+    #ser_ls = serial.tools.list_ports.comports()
 
+    ser.port = '/dev/tty.usbmodem14341'
+    ser.open()
+
+    while True:
+        num=ser.read
+        print(num)
+
+    """
     for i in ser_ls:
         print (i)
         ser.port = i
@@ -19,4 +32,4 @@ if __name__ =='__main__':
         num=ser.read(5)
 
         print(num)
-
+"""
