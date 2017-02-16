c988552fe4f2c15f7ad0ff536dcb32cca1f9ee5c
highhigheggman <highhigheggman@yahoo.co.jp>
2017-02-16 19:58:43 +0900

    update

--- raspberryPi/dataGet.py
+++ raspberryPi/dataGet.py
@@ -5,6 +5,7 @@ import serial
 import serial.tools.list_ports
 import time
 import subprocess
+from operator import itemgetter, attrgetter
 
 if __name__ =='__main__':
 
@@ -18,7 +19,23 @@ if __name__ =='__main__':
     num = 0
 
     # get a list of ports
-    # ser_ls = serial.tools.list_ports.grep(pattern)
+    ser_ls = [i for i in serial.tools.list_ports.comports() if i.location is not None]
+
+    # sort by port location
+    ser_ls.sort(key=attrgetter('location'))
+
+
+    for i in ser_ls:
+        print(i.device)
+        print(i.location)
+
+        #num=ser.readline()
+
+        #print(num)
+
+    # suspend
+    # subprocess.call(["rtcwake", "-m", "mem", "-s", "900"])
+
 
 """
     ser.port = '/dev/cu.wchusbserial14110'
@@ -28,17 +45,5 @@ if __name__ =='__main__':
         if (ser.inWaiting() > 0):
             num = int(ser.readline())
             print(num)
-
-    """
-    for i in ser_ls:
-        print (i)
-        ser.port = i
-        ser.open()
-
-        #num=ser.readline()
-
-        #print(num)
-
-    # suspend
-    # subprocess.call(["rtcwake", "-m", "mem", "-s", "900"])
+"""
 
